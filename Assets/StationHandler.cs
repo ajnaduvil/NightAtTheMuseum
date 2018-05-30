9a0db41c98d093ada9e4b9cb9680018d492efc67
Albin Jose <albin@bmsils.com>
2018-05-30 13:27:30 +0530

    first commit. imported google vr 1.130.1. iTween library from asset store, resonance audio, and 3d modular kit for environment. baked lighting added. Android settings added. basic ui is added.

---
+++ Assets/StationHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationHandler : MonoBehaviour {
+
+
+    public StationHandler nextStation;
+    public StationHandler previousStation;
+
+    public AudioClip narration;
+
+    public void GotoNextStation()
+    {
+
+    }
+    
+    public void GotoPreviousStation()
+    {
+
+    }
+
+    public void StartNarration()
+    {
+
+    }
+
+    public void StopNarration()
+    {
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
